fe4a0cd7933e27e15e674ba2a330e7956cebfa2c
Eric Dobbs <eric@dobbse.net>
2011-09-03 22:24:55 -0600

    remove static site

--- jekyll/temporary/new-layout/resources/enable-css-for-html5-semantic-tags-in-ie.js
+++
@@ -1,9 +0,0 @@
-//why? http://ejohn.org/blog/html5-shiv/
-//who? http://remysharp.com/2009/01/07/html5-enabling-script/
-//where? http://code.google.com/p/html5shiv/
-var e = ("abbr,article,aside,audio,canvas,datalist,details," +
-  "figure,footer,header,hgroup,mark,menu,meter,nav,output," +
-  "progress,section,time,video").split(',');
-for (var i = 0; i < e.length; i++) {
-  document.createElement(e[i]);
-}
