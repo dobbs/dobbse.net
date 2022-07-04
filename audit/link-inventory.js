65fb8fb1e80c3456da154de9ef23c186927b19e4
Eric Dobbs <eric@dobbse.net>
2022-07-04 16:19:41 -0600

    link-inventory.js now exports uniqueKeyForLink()

--- audit/link-inventory.js
+++ audit/link-inventory.js
@@ -4,6 +4,13 @@ import {
 } from "https://deno.land/x/deno_dom/deno-dom-wasm-noinit.ts";
 import {parseAllPosts} from "./parse-all-posts.js"
 
+// Trade-off decision in the definition of the key below.  This
+// code will check links once per page instead of once per
+// run. I'm choosing that trade-off to make it easier to find
+// the pages that need to be edited as a result of this
+// inventory
+export function uniqueKeyForLink({href, post}) {return `${post} -> ${href}`}
+
 export async function linkInventory() {
   await initParser()
   const {parseFromString} = new DOMParser()
@@ -15,14 +22,9 @@ export async function linkInventory() {
     dom.querySelectorAll("a[href]").forEach(link => {
       const href = link.getAttribute("href")
       const label = link.textContent.trim()
-      // Trade-off decision in the definition of the key below.  This
-      // code will check links once per page instead of once per
-      // run. I'm choosing that trade-off to make it easier to find
-      // the pages that need to be edited as a result of this
-      // inventory
-      const key = `${post} -> ${href}`
-      if (!seen.has(key)) {
-        seen.add(key)
+      const uniqueKey = uniqueKeyForLink({href, post})
+      if (!seen.has(uniqueKey)) {
+        seen.add(uniqueKey)
         acc.push({href, label, post})
       }
     })
