be6c4d3f4996d3bb3187cb409c5779e20aabfeee
Eric Dobbs <eric@dobbse.net>
2022-07-04 15:28:01 -0600

    add link-inventory.js
    here I just parse all the jekyll posts and find every link on every
    post.

---
+++ audit/link-inventory.js
@@ -0,0 +1,31 @@
+import {
+  DOMParser,
+  initParser,
+} from "https://deno.land/x/deno_dom/deno-dom-wasm-noinit.ts";
+import {parseAllPosts} from "./parse-all-posts.js"
+
+export async function linkInventory() {
+  await initParser()
+  const {parseFromString} = new DOMParser()
+  const db = parseAllPosts()
+  const seen = new Set()
+  return db.rows.reduce((acc, row) => {
+    const post = row.name
+    const dom = parseFromString(row.body, 'text/html')
+    dom.querySelectorAll("a[href]").forEach(link => {
+      const href = link.getAttribute("href")
+      const label = link.textContent.trim()
+      // Trade-off decision in the definition of the key below.  This
+      // code will check links once per page instead of once per
+      // run. I'm choosing that trade-off to make it easier to find
+      // the pages that need to be edited as a result of this
+      // inventory
+      const key = `${post} -> ${href}`
+      if (!seen.has(key)) {
+        seen.add(key)
+        acc.push({href, label, post})
+      }
+    })
+    return acc
+  }, [])
+}
