63b39853f753e286c27946c1fceb54496c9067ec
Eric Dobbs <eric@dobbse.net>
2022-07-17 21:48:57 -0600

    use pathname to index the Map. remove calls to substring()

--- audit/serve.js
+++ audit/serve.js
@@ -4,24 +4,34 @@ import {stringify} from "https://deno.land/std@0.144.0/encoding/yaml.ts"
 
 let cache = await loadCache()
 let posts = cache.reduce((collection, {href, post, label, details, ok, fetched}) => {
-  let links = collection.get(post) || []
+  const pathname = `/${post}`
+  const links = collection.get(pathname) || []
   links.push({href,label,ok,fetched,details})
-  collection.set(post, links)
+  collection.set(pathname, links)
   return collection
 }, new Map())
 
 serve(async req => {
-  let url = new URL(req.url)
-  console.log({url: url.toString(), pathname: url.pathname})
-  if (url.pathname  == "/") {
-    let html = ''
-    for (let post of [...posts.keys()].sort()) {
-      html += `<p><a href="/${post}">${post}</a></p>\n`
+  let url
+  try {
+    url = new URL(req.url)
+    if (url.pathname  == "/") {
+      let html = ''
+      for (let pathname of [...posts.keys()].sort()) {
+        html += `<p><a href="${pathname}">${pathname}</a></p>\n`
+      }
+      return new Response(html, {headers: {'Content-type': 'text/html'}})
+    } else if (posts.get(url.pathname)) {
+      return new Response(stringify(posts.get(url.pathname)))
+    } else {
+      return new Response(stringify({pathname: url.pathname}))
     }
-    return new Response(html, {headers: {'Content-type': 'text/html'}})
-  } else if (posts.get(url.pathname.substring(1))) {
-    return new Response(stringify(posts.get(url.pathname.substring(1))))
-  } else {
-    return new Response(stringify({pathname: url.pathname}))
+  } catch (error) {
+    return new Response(error.message, {
+      status: 500,
+      statusText: error.message
+    })
+  } finally {
+    console.log({url: url.toString(), pathname: url.pathname})
   }
 })
